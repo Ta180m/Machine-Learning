00073044cabbb84d3cb2ca60af543489a51bcfa3
Anthony Wang <ta180m@gmail.com>
2021-03-25 13:43:22 -0500

    TF is too hard to set up

--- tf-test.py
+++ tf-test.py
@@ -1,15 +1,5 @@
-# TensorFlow and tf.keras
 import tensorflow as tf
-
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-
-# print(tf.__version__)
-
-# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-with tf.device("/GPU:0"):
-    a = tf.random.normal([1, 2])
-
-
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
